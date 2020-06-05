bf8030cc5fb8db880e2205f6ab53f6f8aa8d0572
19else <159753ElSe->
2020-06-05 18:26:37 +0300

    9.1. String [#298650]
    Signed-off-by: 19else <159753ElSe->

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+public class Out {
+
+    public static void main (String[] args) {
+
+        String lirics = "Вот какой рассеянный";
+        System.out.println(lirics);
+
+        byte age = 40;
+        String name = "Сергей";
+        String surname = "Еловиков";
+        String spaceBar = " ";
+        String id = name + spaceBar + surname + spaceBar + age + " лет спустя";
+
+        System.out.println(id);
+
+    }
+}
