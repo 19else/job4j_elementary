a8f31c788beebe7373ad2bbacfd9edffe8028a43
19else <159753ElSe->
2020-06-07 18:57:59 +0300

    0. Что такое тестирование [#298677]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,10 +13,17 @@ public class Converter {
     }
 
     public static void main(String[] args) {
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in), outD = rubleToDollar(in);
+        boolean passed = expected == out;
+        boolean passedD = expected == outD;
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(140);
 
         System.out.println("140 rubles are " + euro + " euro");
+        System.out.println("140 rubles are 2. Test result : " + passed);
         System.out.println("140 rubles are " + dollar + " dollar");
+        System.out.println("140 rubles are 2. Test result : " + passedD);
     }
 }
