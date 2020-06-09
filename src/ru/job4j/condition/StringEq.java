f1832da342963d3c379bd436ae7fe591258e8a83
19else <159753ElSe->
2020-06-09 13:44:10 +0300

    4.1.1. Операторы сравнения в String. [#298606]
    Signed-off-by: 19else <159753ElSe->

---
+++ src/ru/job4j/condition/StringEq.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class StringEq {
+    public static boolean check(String login) {
+        String root = new String("root");
+        return login.equals(root);
+    }
+
+    public static void main(String[] args) {
+        String yourName = "Sergey";
+        boolean userHasAccess = StringEq.check(yourName);
+        System.out.println(userHasAccess);
+
+    }
+
+}
