ca43409bb8c379c095d1815653e3dcbcd500f401
19else <159753ElSe->
2020-06-10 10:25:28 +0300

    4.2. Тернарное сравнение [#298608]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = left > right;
-        return condition ? left : right;
+       // boolean condition = left > right;
+        return left >= right ? left : right;
 
     }
 
