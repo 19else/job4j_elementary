f02ed917f93fb57feff8c2444a6f8a087cafa3e9
19else <159753ElSe->
2020-06-15 15:31:09 +0300

    5.3. Построить шахматную доску в псевдографике. [#298705]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/loop/Board.java
+++ src/ru/job4j/loop/Board.java
@@ -4,7 +4,7 @@ public class Board {
     public static void paint(int width, int height) {
         for (int row = 0; row < height; row++) {
             for (int cell = 0; cell < width; cell++) {
-                if (cell % 2 == 0) {
+                if ((cell + row) % 2 == 0) {
                     System.out.print("X");
                 } else  {
                     System.out.print(" ");
@@ -12,15 +12,11 @@ public class Board {
             }
             /* Добавляем перевод на новую строку */
                 System.out.println();
-                /*Если row четный - ставим пробел*/
-            if (row % 2 == 0) {
-                System.out.print(" ");
-            }
             }
             }
 
     public static void main(String[] args) {
-        paint(5, 5);
+        paint(3, 3);
         System.out.println();
         paint(4, 4);
     }
