4ad022eb0c421cde6613e1c8980202e374895d5e
19else <159753ElSe->
2020-06-16 12:29:16 +0300

    5.5. Простое число [#298696]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -10,8 +10,6 @@ public class CheckPrimeNumber {
             if (number % index == 0)  {
                 prime = false;
                 break;
-            } else {
-                prime = true;
             }
         }
         return prime;
