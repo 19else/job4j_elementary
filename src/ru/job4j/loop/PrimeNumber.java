e9ec2b045612cf71de7de6081ab13bc223e72d38
19else <159753ElSe->
2020-06-16 17:33:29 +0300

    5.6. Простые числа. [#298694]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -4,12 +4,16 @@ public class PrimeNumber {
 
     public int calc(int finish) {
         int count = 0;
-        for (int number = 1; number <= finish; number++) {
+
+        int number = 1;
+        boolean check = CheckPrimeNumber.check(number);
+        for (number = 1; number <= finish; number++) {
             if (CheckPrimeNumber.check(number)) {
 
                 count += 1;
 
             }
+
         }
         return count;
     }
