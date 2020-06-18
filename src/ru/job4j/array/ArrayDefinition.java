b18f7b48e8aaa7eeb40b320c749af1fa0acb7357
19else <159753ElSe->
2020-06-18 17:11:28 +0300

    6.0.2. Заполнение массива. [#298628]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,12 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Sergey Elovikov";
+        names[1] = "Sergey Seleznev";
+        names[2] = "Andrey Petrov";
+        names[3] = "Mike Portnov";
+
+        System.out.println(names[0] + ", " + names[3] + ", "  + names[1] + ", " + names[2]);
     }
 }
