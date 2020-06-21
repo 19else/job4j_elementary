793405aa9e50fd9836e00d31f25c9df6b697b001
19else <159753ElSe->
2020-06-21 21:41:09 +0300

    6.2.3. Перевернуть массив. [#298631] (array.length / 2)
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -4,12 +4,20 @@ public class Turn {
 
     public int[] back(int[] array) {
 
-            for (int index = 0; index < array.length - index - 1; index++) {
-            int second = array[index];
-            array[index] = array[array.length - index - 1];
-            array[array.length - index - 1] = second;
+    //        for (int index = 0; index < array.length - index - 1; index++) {
+    //
+        //            int second = array[index];
+               // array[index] = array[array.length - index - 1];
+                //array[array.length - index - 1] = second;
 
-      }
+            //}
+
+                for (int index = 0; index < array.length / 2; index++) {
+                    int second = array[index];
+                    array[index] = array[array.length - index - 1];
+                    array[array.length - index - 1] = second;
+                }
+
         return array;
     }
 }
