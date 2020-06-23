36bc60d9a78386136bee4f148ef43e60f19c28ea
19else <159753ElSe->
2020-06-23 16:32:56 +0300

    6.4. Слово начинается с ... [#298616]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -4,15 +4,8 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            char convertWord = word[index];
-            word[index] = word[word.length - index - 1];
-            word[word.length - index - 1] = convertWord;
 
-            char convertPost = post[0];
-            post[index] = post[post.length - index - 1];
-            post[post.length - index - 1] = convertPost;
-
-            if (post[index] != word[index]) {
+            if (post[post.length - index - 1] != word[word.length - index - 1]) {
                 result = false;
                 break;
             }
--- test/ru/job4j/array/EndsWithTest.java
+++ test/ru/job4j/array/EndsWithTest.java
@@ -14,4 +14,12 @@ public class EndsWithTest {
         boolean result = EndsWith.endsWith(word, post);
         assertThat(result, is(true));
     }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
 }
