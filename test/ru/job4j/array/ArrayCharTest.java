f17fa5ec163fd9c28ad7aaaa5a2da480eefcc973
19else <159753ElSe->
2020-06-23 12:00:49 +0300

    6.4. Слово начинается с ... [#298616]
    Signed-off-by: 19else <159753ElSe->

---
+++ test/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class ArrayCharTest {
+
+    @Test
+    public void whenStartsWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenStartsWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+}
