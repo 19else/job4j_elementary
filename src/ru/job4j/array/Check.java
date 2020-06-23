aebdd2d633288b3f3a81018236ed7e90be67c5af
19else <159753ElSe->
2020-06-23 09:17:48 +0300

    6.3. Массив заполнен true или false [#298617]
    Signed-off-by: 19else <159753ElSe->

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class Check {
+    @SuppressWarnings("checkstyle:InnerAssignment")
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 0; index < data.length; index++) {
+            if (data[0] != data[index]) {
+                result = false;
+                break;
+            }
+
+            }
+
+        return result;
+        }
+
+      }
+
+
+
---
+++ test/ru/job4j/array/CheckTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import  org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CheckTest {
+
+    @Test
+    public void whenDataMonoByThrueThenTrue() {
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = Check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByThrueThenFalse() {
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = Check.mono(input);
+
+        assertThat(result, is(false));
+    }
+
+}
