2c1824f93f43284629b82a572277761a9b8d4d2f
19else <159753ElSe->
2020-07-20 11:11:34 +0300

    4. Расстояние между точками в трехмерном пространстве. [#298783]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/conditional/Point.java
+++ src/ru/job4j/conditional/Point.java
@@ -7,26 +7,57 @@ public class Point {
      * Это поле объекта. Оно доступно только конкретному объекту
      */
     private int x;
-
     /**
      * Это поле объекта. Оно доступно только конкретному объекту.
      */
     private int y;
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int z;
 
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     * @param x координата x
+     * @param y координата y
+     * @param z координата z
+     */
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public double distance3d(Point that) {
+        return sqrt(pow(that.x - this.x, 2) + (pow(that.y - this.y, 2) + (pow(that.z - this.z, 2))));
+    }
+
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
+        Point x = new Point(0, 0, 0);
+        Point y = new Point(0, 0, 0);
+        Point z = new Point(1, 1, 1);
         double dist = a.distance(b);
-        System.out.println(dist);
+        double temp = x.distance3d(y);
+        double dist3d = z.distance3d(y);
+        System.out.println("dist = " + dist);
+        System.out.println("temp = " + temp);
+        System.out.println("dist3d = " + dist3d);
     }
 
     public static double distance(int x1, int y1, int x2, int y2) {
