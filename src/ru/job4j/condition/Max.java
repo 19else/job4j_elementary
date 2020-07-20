49f7097bb7345b7fe98410f8bdfaf80948a5aefa
19else <159753ElSe->
2020-07-20 08:56:57 +0300

    3. Перегрузить метод max для трех чисел [#298784]
    Signed-off-by: 19else <159753ElSe->

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,13 +1,26 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-       // boolean condition = left > right;
-        return left >= right ? left : right;
+
+    public static int max(int first, int second, int third) {
+          return Math.max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int temp = max(second, third, fourth);
+        return Math.max(first, temp);
+    }
+
+    public static int max(int first, int second) {
+           return first >= second ? first : second;
     }
 
     public static void main(String[] args) {
-        int result = Max.max(1, 2);
-        System.out.println("Максимальное число " + result);
+        int result = Max.max(3, 4);
+        int max = Max.max(1, 2, 3, 4);
+        int maxThird = Max.max(9, 8, 7);
+        System.out.println("Максимальное число из двух значений - " + result);
+        System.out.println("Максимальное число из трех значений - " + max);
+        System.out.println("Максимальное число из четырех значений - " + maxThird);
     }
 }
---
+++ src/ru/job4j/excersises/CrossAway.java
@@ -0,0 +1,21 @@
+package ru.job4j.excersises;
+
+public class CrossAway {
+    public static void main(String[] args) {
+        int[] left = {1, 2, 3, 4, 5};
+        int[] right = {3, 1, 5};
+
+        for (int indexLeft = 0; indexLeft < left.length; indexLeft++) {
+            for (int indexRight = 0; indexRight < right.length; indexRight++) {
+                if (left[indexLeft] == right[indexRight]) {
+                    System.out.println(left[indexLeft]);
+                    break;
+                }
+            }
+            }
+        }
+
+    }
+
+
+
